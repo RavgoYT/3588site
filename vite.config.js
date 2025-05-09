718ef6dc096375c7de4426e178ad61aff62165e9
RavgoYT <88573982+RavgoYT@users.noreply.github.com>
2025-05-08 21:10:40 -0700

    gh-pages setup using package.json
    instead of github actions

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/3588site/',
+  base: '/3588site',
   server: {
     host: true
   }
