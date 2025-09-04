01f3a5cb115d476d8cf2033f7d00f7056f925374
Eddy Walsh <walsheddy5@gmail.com>
2025-09-03 20:00:49 -0700

    That moment u realize u didnt make sure changes worked on mobile

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -4,12 +4,12 @@ import { Instagram, Youtube, Mail } from "lucide-react";
 const ContactSection = () => {
 	return (
 		<section id="contact" className="pb-5 bg-black">
-			<div className="container mx-auto px-8 text-center flex flex-row justify-center gap-8">
+			<div className="container mx-auto px-8 text-center flex flex-col md:flex-row justify-center gap-8">
 				<a
 					href="mailto:projectmanager@lindberghrobotics.org"
 					target="_blank"
 					rel="noopener noreferrer"
-					className="min-w-[200px] mt-6 inline-block px-8 py-4 rounded-lg font-bold text-white transition-all duration-300 hover:scale-105 hover:shadow-xl"
+					className="md:min-w-[200px] mt-6 inline-block px-8 py-4 rounded-lg font-bold text-white transition-all duration-300 hover:scale-105 hover:shadow-xl"
 					style={{
 						background: "linear-gradient(90deg, #6586c7, #e23942)",
 						letterSpacing: "0.1em",
@@ -24,7 +24,7 @@ const ContactSection = () => {
 					href="https://www.instagram.com/lhseagles_robotics?utm_source=ig_web_button_share_sheet&igsh=dnJkcHJqenI1bWxt"
 					target="_blank"
 					rel="noopener noreferrer"
-					className="mt-6 inline-block min-w-[200px] px-8 py-4 rounded-lg font-bold text-white transition-all duration-300 hover:scale-105 hover:shadow-xl"
+					className="mt-6 inline-block md:min-w-[200px] px-8 py-4 rounded-lg font-bold text-white transition-all duration-300 hover:scale-105 hover:shadow-xl"
 					style={{
 						background: "linear-gradient(90deg, #6586c7, #e23942)",
 						letterSpacing: "0.1em",
